a3d677361b64da8d3a663c2b97669a5d3d8d7a3f
nemitha2005 <nemithan05@gmail.com>
2025-02-12 00:06:17 +0530

    chore: move firebase config to api folder and update auth

--- api/config/firebase.ts
+++ api/config/firebase.ts
@@ -1,4 +1,7 @@
-const firebaseConfig = {
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  const firebaseConfig = {
     apiKey: "AIzaSyDeBAqbYbQSoDYruvYZ_cVdJ8dbfOTGRRE",
     authDomain: "vatelanka-e6828.firebaseapp.com",
     projectId: "vatelanka-e6828",
@@ -7,5 +10,9 @@ const firebaseConfig = {
     appId: "1:444350594980:web:38660abe178b78750b4b31",
     measurementId: "G-0C4KQG6Q61"
   };
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
   
-  export default firebaseConfig;
+  return res.json(firebaseConfig)
+}
